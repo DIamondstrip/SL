6df80cb28b2965dbd37e46f45fb11088b9f24835
1786773981 <1786773981@qq.com>
2019-11-12 20:32:38 +0800

    吕俊杰11.12

---
+++ shuangliao/src/views/mine/myGift.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="myGift">
+    <div>
+      <Test>我的礼物</Test>
+    </div>
+    <van-tabs v-model="activeName">
+      <hr />
+      <!-- Tab切换=收到礼物 -->
+      <van-tab title="收到" name="getGift">
+        <!-- 礼物 -->
+        <div class="getG">
+          <ul>
+            <!-- 此处数据应遍历赋值（li标签） -->
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+            <li>
+              <span class="span1">
+                <van-icon name="point-gift" />
+              </span>
+              <span class="span2">火箭</span>
+              <span class="span3">6</span>
+            </li>
+          </ul>
+        </div>
+        <!-- 提示信息 -->
+        <div class="msgG">
+          <hr />
+          <div>
+            <p class="p1">如何获得礼物：</p>
+            <p class="p2">视频、文字聊天、别人访问你的主页时，都有机会获得</p>
+          </div>
+          <hr />
+        </div>
+        <!-- 赠送者 -->
+        <div class="manG">
+          <ul>
+            <!-- 此处数据应遍历赋值（li标签） -->
+            <li>
+              <div class="mgLi">
+                <!-- 头像 -->
+                <div class="mgImg">
+                  <img src />
+                </div>
+                <!-- 个人信息 -->
+                <div class="mgMsg">
+                  <p class="msg1">果捏捏</p>
+                  <p class="msg2">
+                    <i>赠送</i>
+                    <span>XXXXX礼物</span>
+                  </p>
+                </div>
+                <div class="msgDate">2019/8/12</div>
+              </div>
+              <hr />
+            </li>
+          </ul>
+        </div>
+      </van-tab>
+      <!-- Tab切换=送出礼物 -->
+      <van-tab title="送出" name="putGift">
+        <ul>
+          <!-- 此处数据应遍历赋值（li标签） -->
+          <li>
+            <div class="mgLi">
+              <!-- 头像 -->
+              <div class="mgImg">
+                <img src />
+              </div>
+              <!-- 个人信息 -->
+              <div class="mgMsg">
+                <p class="msg1">果捏捏</p>
+                <p class="msg2">
+                  <i>赠送</i>
+                  <span>XXXXX礼物</span>
+                </p>
+              </div>
+              <div class="msgDate">2019/8/12</div>
+            </div>
+            <hr />
+          </li>
+        </ul>
+      </van-tab>
+    </van-tabs>
+  </div>
+</template>
+
+<script>
+import Test from "../../components/Test";
+export default {
+  name: "sendGift",
+  data() {
+    return {
+      activeName: ""
+    };
+  },
+  methods: {},
+  created() {
+    this.activeName = this.$route.params.name;
+  },
+  computed: {},
+  components: {
+    Test
+  }
+};
+</script>
+
+<style scoped>
+hr {
+  margin: 0;
+}
+/* 礼物 */
+.getG ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+.getG ul li {
+  width: 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
+}
+.getG .span1 {
+  font-size: 30px;
+  display: inline-block;
+  height: 35px;
+}
+.getG .span2 {
+  color: #999;
+  font-size: 12px;
+}
+.getG .span3 {
+  color: #ff3e5d;
+  font-size: 8px;
+}
+/* 提示信息 */
+.msgG div {
+  padding: 0 20px;
+}
+.msgG .p1 {
+  color: black;
+  font-size: 12px;
+  line-height: 30px;
+}
+.msgG .p2 {
+  color: #999;
+  font-size: 12px;
+  line-height: 30px;
+}
+/* 赠送者 */
+.mgLi {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+}
+.mgLi p {
+  line-height: 20px;
+}
+.mgImg {
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+  border-radius: 50%;
+}
+.msg1 {
+  color: black;
+  font-size: 16px;
+}
+.msg2 i {
+  color: #999;
+  font-size: 14px;
+}
+.msg2 span {
+  color: #80a3fe;
+  font-size: 14px;
+  margin-left: 5px;
+}
+.mgMsg {
+  margin-left: -80px;
+}
+.msgDate {
+  color: #999;
+  font-size: 14px;
+}
+</style>
--- shuangliao/src/views/mine/photo.vue
+++ shuangliao/src/views/mine/photo.vue
@@ -1,19 +1,75 @@
 <template>
   <div class="photo">
-    <h1>个人相册</h1>
+    <Test>我的相册</Test>
+    <div class="upload">
+      <van-uploader :after-read="afterRead1" v-model="fileList1" :max-count="3">
+        <p class="photoP">
+          <span class="span1">
+            <van-icon name="plus" color="black" />
+          </span>
+          <span class="span2">上传照片</span>
+        </p>
+      </van-uploader>
+      <van-uploader :after-read="afterRead2" v-model="fileList2" :max-count="1">
+        <p class="photoP">
+          <span class="span1">
+            <van-icon name="plus" color="black" />
+          </span>
+          <span class="span2">形象视频</span>
+        </p>
+      </van-uploader>
+    </div>
   </div>
 </template>
 
 <script>
+import Test from "../../components/Test";
 export default {
   name: "photo",
   data() {
-    return {};
+    return {
+      fileList1: [],
+      fileList2: []
+    };
   },
-  methods: {},
-  computed: {}
+  methods: {
+    afterRead1(file) {
+      // 此时可以自行将文件上传至服务器
+      console.log(file);
+      this.fileList1.push({ url: "file.content", isImage: true });
+    },
+    afterRead2(file) {
+      // 此时可以自行将文件上传至服务器
+      console.log(file);
+      this.fileList2.push({ url: "file.content", isImage: true });
+    }
+  },
+  computed: {},
+  components: {
+    Test
+  }
 };
 </script>
 
 <style scoped>
+.upload {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.photoP {
+  width: 80px;
+  height: 80px;
+  background: #e5e5e5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 2px;
+}
+.span1 {
+  margin-top: 10px;
+}
+.span2 {
+  font-size: 16px;
+}
 </style>
