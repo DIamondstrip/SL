ff472369859c7c1bd7c72bc2bfc3176448475b94
Diamondstrip <943116216@qq.com>
2019-11-13 10:12:57 +0800

    11-13zy

--- shuangliao/src/views/mine/myGift.vue
+++ shuangliao/src/views/mine/myGift.vue
@@ -142,7 +142,7 @@ export default {
   },
   methods: {},
   created() {
-    this.activeName = this.$route.params.name;
+    this.activeName = this.$route.params.giftName;
   },
   computed: {},
   components: {
