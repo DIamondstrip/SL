dc7b237e32259e9b18265a3504f8d064bcf022f9
evilspiritss <1916200869@qq.com>
2019-11-14 10:26:58 +0800

    cdd

--- shuangliao/vue.config.js
+++ shuangliao/vue.config.js
@@ -2,6 +2,22 @@ const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
 
 module.exports = {
+     // 服务端代理，解决跨域
+     devServer: {
+        proxy: {
+            '/sms': {
+                target: 'http://fgserver.top',//代理的接口域名
+                ws: true,
+                changeOrigin: true //允许跨域
+            },
+            '/user':{
+                target: 'http://fgserver.top',//代理的接口域名
+                ws: true,
+                changeOrigin: true //允许跨域
+            }
+        }
+    },
+
     css: {
         loaderOptions: {
             postcss: {
